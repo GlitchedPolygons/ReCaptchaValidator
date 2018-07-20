a1e15a0eeeb8b670be3427bcb4768444dca8c277
Raphael Beck <glitchedpolygons@gmail.com>
2018-07-20 03:44:15 +0200

    Extracted ReCaptcha validator code from Glitched Polygons website into reusable NuGet package.

---
+++ ReCaptchaValidator/IReCaptchaValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace GlitchedPolygons.Services.ReCaptchaValidator
+{
+    /// <summary>
+    /// Interface for validating whether a user is human or not.
+    /// </summary>
+    public interface IReCaptchaValidator
+    {
+        /// <summary>
+        /// Submits the reCAPTCHA code that you got back from a reCAPTCHA-enabled form
+        /// to the Google servers and returns the response (deserialized JSON).
+        /// </summary>
+        /// <param name="code">The generated reCAPTCHA validation code from the form.</param>
+        /// <param name="ip">The request's IP Address.</param>
+        /// <returns>Deserialized JSON of Google's response to the submitted reCAPTCHA code.</returns>
+        Task<ReCaptchaResponse> Validate(string code, IPAddress ip);
+    }
+}
+
+// Copyright (C) - Raphael Beck, 2018
---
+++ ReCaptchaValidator/ReCaptchaResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace GlitchedPolygons.Services.ReCaptchaValidator
+{
+    /// <summary>
+    /// Google ReCaptchaCode Response JSON
+    /// </summary>
+    public struct ReCaptchaResponse
+    {
+        /// <summary>
+        /// Bot or not?
+        /// </summary>
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+        /// </summary>
+        [JsonProperty("challenge_ts")]
+        public DateTime TimeStamp { get; set; }
+
+        /// <summary>
+        /// The hostname of the site where the reCAPTCHA was solved
+        /// </summary>
+        [JsonProperty("hostname")]
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// Any eventual error codes.
+        /// </summary>
+        [JsonProperty("error-codes")]
+        public string[] ErrorCodes { get; set; }
+    }
+}
+
+// Copyright (C) - Raphael Beck, 2018
